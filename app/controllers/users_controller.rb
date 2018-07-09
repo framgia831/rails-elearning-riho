f0753c87c2f7dd7752ed0eaaf9650b1ac2f50077
uminonar <rihoohkubo@gmail.com>
2018-07-09 15:17:05 +0800

    Implement user authentication and relationships

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,75 @@
+class UsersController < ApplicationController
+  before_action :require_login, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 15)
+  end
+  
+  def new
+    @user = User.new
+  end 
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      log_in user
+      flash[:success] = "Welcome to the E-leaning System"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash.now[:success] = "Updated your information successfully."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    flash[:success] = "User deleted."
+    user.destroy
+    redirect_to users_path
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def require_login
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_path
+    end
+  end
+
+    # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
+  end
+
+  # 管理者かどうか確認
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
+  get '/lesson', to: 'static_pages#lesson'
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships,       only: [:create, :destroy]
+
 end
