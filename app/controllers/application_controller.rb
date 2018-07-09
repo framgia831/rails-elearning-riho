b59183e67b370e9129a17de0a0fce15b55a04f38
Riho <nupicker@gmail.com>
2018-07-09 17:37:16 +0800

    Implement user authentication and relationships (#8)
    * Implement user authentication and relationships
    
    * users_controller.rb:user -> @user, config/application.rb: erase end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+  private
+  def require_login
+  	store_location
+  	flash[:danger] = "Please log in."
+  	redirect_to login_path
+  end
 end
+  
